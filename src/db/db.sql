f2928e0b1c1cc353594b1d867e43cfa00ef0f136
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-06-03 15:04:14 -0300

    feat: added CreatePostRequest and CreateUserRequest

---
+++ src/db/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE quarkus-social;
+
+CREATE TABLE USERS(
+                      id bigserial not null primary key,
+                      name varchar(100) not null,
+                      age integer not null
+);
+
+
+CREATE TABLE POSTS(
+    id bigserial not null primary key,
+    post_text varchar(150) not null,
+    dateTime timestamp,
+    user_id bigint not null references USERS(id)
+)
+
+CREATE TABLE FOLOWERS(
+                         id BIGSERIAL NOT NULL PRIMARY KEY,
+                         user_id bigint not null references USERS(id),
+                         follower_id bigint not null references USERS(id)
+);
